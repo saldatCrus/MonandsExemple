74e3db5960305da1e1a813ebef7a903435dfb23b
saldatCrus <dimakoval0198@gmail.com>
2021-11-12 02:16:07 +0300

    Добавьте файлы проекта.

---
+++ MonandsExemple/Models/TestModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MonandsExemple.Models
+{
+    public class TestModel
+    {
+        public int Id { get; set; }
+        
+        public KekwModel Kekw { get; set; }
+            
+    }
+}
---
+++ MonandsExemple/Program.cs
@@ -0,0 +1,33 @@
+﻿using MonandsExemple;
+using MonandsExemple.Models;
+
+public class Program
+{
+    public static async  Task Main(string[] args)
+    {
+        TestModel testModel1 = new();
+
+        TestModel testModel2 = new();
+
+        testModel1.Kekw = new();
+
+        testModel1.Kekw.Name = "KEKL";
+
+        testModel2.Kekw = new();
+
+        testModel2.Kekw.Name = "Kik yore ass back heare";
+
+        Console.WriteLine(TestMaybeScenario(testModel2, testModel1));
+
+    }
+
+    public static string TestMaybeScenario(TestModel errModel, TestModel fineModel) 
+    {
+        string result = fineModel.Kekw.Name;
+
+        result = errModel.With(p => p.Kekw)
+            .With(p => p.Name).Return(x => x, result);
+        
+        return result;
+    } 
+}
